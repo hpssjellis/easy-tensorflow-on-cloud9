4966725e79536b269ffeec8969cf963445db98fd
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 07:49:56 -0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash setup.sh
+
+
+echo "First checking the Python version"
+python --version
+
+echo "Now checking is pip is installed"
+
+pip list
+
+echo "next list this directory"
+pwd
+ls -lah  
+
