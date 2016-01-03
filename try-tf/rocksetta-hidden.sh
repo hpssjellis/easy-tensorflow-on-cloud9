9ceb7172f051575ee2c93324277fedf456ce2af2
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 15:20:05 -0800

    Update rocksetta-hidden.sh

--- try-tf/rocksetta-hidden.sh
+++ try-tf/rocksetta-hidden.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-source ~/tensorflow/bin/activate
+source ~/virtual-tf/bin/activate
 python hidden.py --train simdata/moon_data_train.csv --test simdata/moon_data_eval.csv --num_epochs 100 --num_hidden 3
