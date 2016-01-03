f117197bd94c09173eac4aed2f7abc28807c8889
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 12:48:36 -0800

    Update rocksetta03.sh

--- rocksetta-examples/rocksetta03.sh
+++ rocksetta-examples/rocksetta03.sh
@@ -1,3 +1,10 @@
 #!/bin/bash  
 source ~/tensorflow/bin/activate
-tensorboard --logdir=~/tmp --host $IP --port $PORT
+tensorboard --logdir=/home/ubuntu/workspace/tmp5 --host $IP --port $PORT
+
+
+
+
+
+# assumes something like the following in your code
+# summary_writer = tf.train.SummaryWriter('/home/ubuntu/workspace/tmp5', sess.graph_def)
