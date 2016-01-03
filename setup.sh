3592a6a74a77413bafdbf9206cfa0b5947b2cd92
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 09:05:10 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ echo "."
 
 echo "Now installing TensorFlow"
 
-pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
 
 
 echo"--------------------------------------------------------------"
