fd190cfe4b01561fb1aad66dc4d9cbb97025bfc8
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 08:11:21 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,11 +7,31 @@
 echo "First checking the Python version"
 python --version
 
+echo"--------------------------------------------------------------"
+echo " "
+
+
 echo "Now checking is pip is installed"
 
 pip list
+
+echo"--------------------------------------------------------------"
+echo " "
+
 
 echo "next list this directory"
 pwd
 ls -lah  
 
+echo"--------------------------------------------------------------"
+echo " "
+
+
+
+echo "Now installing TensorFlow"
+
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
+
+
+echo"--------------------------------------------------------------"
+echo " "
