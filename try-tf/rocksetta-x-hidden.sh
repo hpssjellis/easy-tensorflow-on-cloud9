f94750450117652343d42fb621e6880cf41de002
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 15:19:21 -0800

    Update and rename rocksetta-02-hidden.sh to rocksetta-x-hidden.sh

--- try-tf/rocksetta-x-hidden.sh
+++ try-tf/rocksetta-x-hidden.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
-source ~/tensorflow/bin/activate
+source ~/virtual-tf/bin/activate
 python hidden.py --train simdata/saturn_data_train.csv --test simdata/saturn_data_eval.csv --num_epochs 50 --num_hidden 2
