8a1d6afd2e8e544756283fb52615366310eddcf2
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 09:41:21 -0800

    Update rocksetta01.sh

--- rocksetta-examples/rocksetta01.sh
+++ rocksetta-examples/rocksetta01.sh
@@ -1,10 +1,3 @@
 #!/bin/bash  
 source ~/tensorflow/bin/activate
-python
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
-a = tf.constant(10)
-b = tf.constant(32)
-print(sess.run(a + b))
+hello42.py
