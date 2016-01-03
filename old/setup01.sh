db548fe2a77b6823f740844ff5d90d1d922eab28
Jeremy Ellis <jellis@hpssapps.com>
2016-01-03 07:42:17 -0800

    old

---
+++ old/setup01.sh
@@ -0,0 +1,19 @@
+#!/bin/bash  
+
+echo "This is a shell script"  
+
+ls -lah  
+
+echo "I am done running ls"  
+
+SOMEVAR='text stuff'  
+
+echo "$SOMEVAR"  
+
+
+git clone --recurse-submodules https://github.com/tensorflow/tensorflow
+
+
+
+
+echo "run by bash myFilename.sh"
