c27787eda7798242438cfc23c3918d0475173f33
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:43:10 -0800

    Update a04-rocksetta-hidden.sh

--- bcomposes-examples/a04-rocksetta-hidden.sh
+++ bcomposes-examples/a04-rocksetta-hidden.sh
@@ -1,3 +1,20 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
+
+
+
+
+
+
+
+echo "running python hidden.py --train simdata/saturn_data_train.csv --test simdata/saturn_data_eval.csv --num_epochs 50 --num_hidden 2"
+echo "from the bcomposes-examples folder"
+echo ""
+
 python hidden.py --train simdata/saturn_data_train.csv --test simdata/saturn_data_eval.csv --num_epochs 50 --num_hidden 2
+
+
+
+
+echo ""
+echo "--------------------Done--------------------------"
