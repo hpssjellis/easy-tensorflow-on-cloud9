c89db76213e7744bc3326d850be83af43a84cab0
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:34:56 -0800

    Update a02-rocksetta-good2d.sh

--- rocksetta-examples/a02-rocksetta-good2d.sh
+++ rocksetta-examples/a02-rocksetta-good2d.sh
@@ -1,5 +1,14 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
+
+
+
+
+echo "running python good2d.py from the rocksetta-examples folder"
+echo ""
+echo ""
+
 python good2d.py
 
-
+echo ""
+echo "--------------------Done--------------------------"
