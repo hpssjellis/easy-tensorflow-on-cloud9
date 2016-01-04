83198adef7f5becfc3ee8dbdba583364d044468e
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:38:27 -0800

    Update a03-rocksetta-tensorboard.sh

--- rocksetta-examples/a03-rocksetta-tensorboard.sh
+++ rocksetta-examples/a03-rocksetta-tensorboard.sh
@@ -1,9 +1,21 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
+
+
+
+
+
+echo "running tensorboard from the pip folders"
+echo ""
+echo ""
+
 tensorboard --logdir=/home/ubuntu/workspace/tmp5 --host $IP --port $PORT
 
-
-
+echo "On cloud9 run the menu item preview-->preview running application "
+echo "To see the tensorBoard website"
+echo "Check that the .py file uses /home/ubuntu/workspace/tmp5 instead of the /tmp directory"
+echo "The /tmp folder may work, but is hidden from the main cloud9 folders"
+echo "--------------------Done--------------------------"
 
 
 
