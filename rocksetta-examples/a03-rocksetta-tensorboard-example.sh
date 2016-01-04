3686ccd9d3b58a3bacb9b439899af9f1429a62d2
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-04 11:23:47 -0800

    Update and rename a04-rocksetta-tensorboard-example.sh to a03-rocksetta-tensorboard-example.sh

--- rocksetta-examples/a03-rocksetta-tensorboard-example.sh
+++ rocksetta-examples/a03-rocksetta-tensorboard-example.sh
@@ -14,13 +14,15 @@ echo ""
 tensorboard --logdir=/home/ubuntu/workspace/tmp5 --host $IP --port $PORT
 
 
-python tensorboard-example.py
 
 
 echo "On cloud9 run the menu item preview-->preview running application "
 echo "To see the tensorBoard website"
 echo "Check that the .py file uses /home/ubuntu/workspace/tmp5 instead of the /tmp directory"
 echo "The /tmp folder may work, but is hidden from the main cloud9 folders"
+
+
+echo "TO SEE TENSORBOARD YOU MUST NOW ALSO RUN A04-ROCKSETTA-TENSORBOARD.SH"
 echo "--------------------Done--------------------------"
 
 
