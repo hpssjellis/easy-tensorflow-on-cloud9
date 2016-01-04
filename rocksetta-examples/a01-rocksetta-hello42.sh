696515470b666ac71f5068b4f7eb482b2e5bbe0e
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:34:24 -0800

    Update a01-rocksetta-hello42.sh

--- rocksetta-examples/a01-rocksetta-hello42.sh
+++ rocksetta-examples/a01-rocksetta-hello42.sh
@@ -1,3 +1,11 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
+
+echo "running python hello42.py from the rocksetta-examples"
+echo ""
+echo ""
+
 python hello42.py
+
+echo ""
+echo "--------------------Done--------------------------"
