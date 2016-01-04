78e3d9632f594a112831268268d28aa20cf9446d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:41:43 -0800

    Update a02-rocksetta-softmax.sh

--- bcomposes-examples/a02-rocksetta-softmax.sh
+++ bcomposes-examples/a02-rocksetta-softmax.sh
@@ -1,3 +1,17 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
+
+
+
+
+
+echo "running python softmax.py --train simdata/linear_data_train.csv --test simdata/linear_data_eval.csv"
+echo "from the bcomposes-examples folder"
+echo ""
+
+
 python softmax.py --train simdata/linear_data_train.csv --test simdata/linear_data_eval.csv
+
+
+echo ""
+echo "--------------------Done--------------------------"
