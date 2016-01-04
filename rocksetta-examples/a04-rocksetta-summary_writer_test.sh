9b242ec19febf10b6d14144b100046ee47d446b9
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:39:13 -0800

    Update a04-rocksetta-summary_writer_test.sh

--- rocksetta-examples/a04-rocksetta-summary_writer_test.sh
+++ rocksetta-examples/a04-rocksetta-summary_writer_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
-
+echo "Running true tensorFlow examples!"
 echo "running summary_writer_test.py in the folder /home/ubuntu/workspace/tensorflow/tensorflow/python/training"
 echo ""
 echo ""
