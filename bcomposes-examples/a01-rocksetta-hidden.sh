a84b446423a24d7b89ca39225e78b136c39b2a4d
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-03 23:41:13 -0800

    Update a01-rocksetta-hidden.sh

--- bcomposes-examples/a01-rocksetta-hidden.sh
+++ bcomposes-examples/a01-rocksetta-hidden.sh
@@ -1,3 +1,15 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
-python hidden.py --train simdata/moon_data_train.csv --test simdata/moon_data_eval.csv --num_epochs 100 --num_hidden 3
+
+
+
+
+echo "running python hidden.py --train simdata/moon_data_train.csv --test simdata/moon_data_eval.csv --num_epochs 100 --num_hidden 3"
+echo "from the bcomposes-examples folder"
+echo ""
+
+python hello42.pypython hidden.py --train simdata/moon_data_train.csv --test simdata/moon_data_eval.csv --num_epochs 100 --num_hidden 3
+
+
+echo ""
+echo "--------------------Done--------------------------"
