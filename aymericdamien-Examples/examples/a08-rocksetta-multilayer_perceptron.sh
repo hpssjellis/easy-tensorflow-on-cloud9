f549bba6637f36190878c6924be722f3c43ae726
Jeremy Ellis <jellis@hpssapps.com>
2016-01-03 22:51:55 -0800

    new naming

---
+++ aymericdamien-Examples/examples/a08-rocksetta-multilayer_perceptron.sh
@@ -0,0 +1,3 @@
+#!/bin/bash  
+source ~/virtual-tf/bin/activate
+python multilayer_perceptron.py
--- rocksetta-examples/a03-rocksetta-tensorboard.sh
+++ rocksetta-examples/a03-rocksetta-tensorboard.sh
@@ -6,5 +6,6 @@ tensorboard --logdir=/home/ubuntu/workspace/tmp5 --host $IP --port $PORT
 
 
 
+
 # assumes something like the following in your code
 # summary_writer = tf.train.SummaryWriter('/home/ubuntu/workspace/tmp5', sess.graph_def)
