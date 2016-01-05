1f87ea3e21fafcfa2e63f17aa6eb8c4511a1cd9c
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-04 20:02:46 -0800

    Update and rename a07-rocksetta- to a07-rocksetta-myImage.sh

--- rocksetta-examples/a07-rocksetta-myImage.sh
+++ rocksetta-examples/a07-rocksetta-myImage.sh
@@ -2,10 +2,10 @@
 source ~/virtual-tf/bin/activate
 echo "Running true tensorFlow examples!"
 echo "running summary_writer_test.py in the folder /home/ubuntu/workspace/tensorflow/tensorflow/python/training"
-echo ""
+echo "Trying to compare my own image from this folder called myImage.jpg"
 echo ""
 
-python /home/ubuntu/workspace/tensorflow/tensorflow/python/training/summary_writer_test.py
+python /home/ubuntu/workspace/tensorflow/tensorflow/python/training/summary_writer_test.py --image_file=myImage.jpg
 
 echo ""
 echo "--------------------Done--------------------------"
