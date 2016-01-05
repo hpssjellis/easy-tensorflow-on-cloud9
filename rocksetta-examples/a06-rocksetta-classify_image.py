628876f68ed843e0aeaba00e268a094e32eaf857
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-04 19:57:42 -0800

    Update and rename a06-rocksetta- to a06-rocksetta-classify_image.py

---
+++ rocksetta-examples/a06-rocksetta-classify_image.py
@@ -0,0 +1,11 @@
+#!/bin/bash  
+source ~/virtual-tf/bin/activate
+echo "Running true tensorFlow examples!"
+echo "running classify_image.py.py in the folder /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet"
+echo ""
+echo ""
+
+python /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet/classify_image.py.py
+
+echo ""
+echo "--------------------Done--------------------------"
