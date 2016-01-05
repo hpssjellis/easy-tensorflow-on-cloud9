73fbbde59140d5e4278ca4621a9a98b556bee316
Jeremy Ellis <keyfreemusic@gmail.com>
2016-01-04 20:03:25 -0800

    Update a06-rocksetta-classify_image.sh

--- rocksetta-examples/a06-rocksetta-classify_image.sh
+++ rocksetta-examples/a06-rocksetta-classify_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 source ~/virtual-tf/bin/activate
 echo "Running true tensorFlow examples!"
-echo "running classify_image.py.py in the folder /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet"
+echo "running classify_image.py in the folder /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet"
 echo ""
 echo ""
 
-python /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet/classify_image.py.py
+python /home/ubuntu/workspace/tensorflow/tensorflow/models/image/imagenet/classify_image.py
 
 echo ""
 echo "--------------------Done--------------------------"
