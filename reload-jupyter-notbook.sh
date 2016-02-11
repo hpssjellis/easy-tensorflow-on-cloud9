451320a72e87c4c655b35b0ac66433c72c79a79e
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-10 20:35:11 -0800

    Create reload-jupyter-notbook.sh

---
+++ reload-jupyter-notbook.sh
@@ -0,0 +1,12 @@
+
+
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash reload-jupyter-notbook.sh
+
+
+
+
+
+jupyter notebook --ip=0.0.0.0 --port=8080 --no-browser
