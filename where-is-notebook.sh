b9509c55608afc5b5c320f3c810a278b1423701e
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-11 13:20:57 -0800

    Update where-is-notebook.sh

--- where-is-notebook.sh
+++ where-is-notebook.sh
@@ -1,4 +1,10 @@
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash where-is-notebook.sh
+
 
 
 
 echo "http://0.0.0.0:8080/"
+echo "Click this windoe, Then click the link above this line, Then open"
